27f058004b9ec88b71c106fcc7d3afdb16599a12
Haris <haris.shaqiri@outlook.com>
2023-02-22 14:37:20 +0100

    Final Product

--- main.js
+++ main.js
@@ -17,3 +17,64 @@ $(document).ready(function(){
     }
   });
 });
+
+
+function updateClock() {
+        const now = new Date();
+        const hours = now.getHours();
+        const minutes = now.getMinutes();
+        const seconds = now.getSeconds();
+        const timeString = `${hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+        document.getElementById("clock").textContent = timeString;
+      }
+      
+      updateClock();
+      setInterval(updateClock, 1000);
+
+let suggestions = [
+    "phones",
+    "monitors",
+    "tv",
+    "mouses",
+    "heaphones",
+    "keyboards",
+];
+
+const searchInput = document.querySelector(".searchInput");
+const input = searchInput.querySelector("input");
+const resultBox = searchInput.querySelector(".resultBox");
+const icon = searchInput.querySelector(".icon");
+let linkTag = searchInput.querySelector("a");
+let webLink;
+
+input.onkeyup = (e)=>{
+    let userData = e.target.value;
+    let emptyArray = [];
+    if(userData){
+        emptyArray = suggestions.filter((data)=>{
+            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase()); 
+        });
+        emptyArray = emptyArray.map((data)=>{
+            return data = '<li>'+ data +'</li>';
+        });
+        searchInput.classList.add("active");
+        showSuggestions(emptyArray);
+        let allList = resultBox.querySelectorAll("li");
+        for (let i = 0; i < allList.length; i++) {
+            allList[i].setAttribute("onclick", "select(this)");
+        }
+    }else{
+        searchInput.classList.remove("active");
+    }
+}
+
+function showSuggestions(list){
+    let listData;
+    if(!list.length){
+        userValue = inputBox.value;
+        listData = '<li>'+ userValue +'</li>';
+    }else{
+        listData = list.join('');
+    }
+    resultBox.innerHTML = listData;
+}
